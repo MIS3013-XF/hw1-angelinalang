af2c4fac408ee90ef034d7305b41d02e172d421d
angelinalang <angelinalang@ou.edu>
2025-09-08 10:25:51 -0500

    Draft #1

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,6 +1,6 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
+// Your Name: Angelina Q. Lang
 // Did you seek help ? If yes, specify the helper or web link here: 
 
 using System;
@@ -15,6 +15,43 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+            // Task 1: Sale Total
+
+            // Declare: variables of this problem and their end format
+            string Item;
+            double Quantity;
+            double Price;
+            double Subtotal;
+            const double TaxRate = .085;
+            double SalesTax;
+            double Total;
+
+            // Prompt: Ask the user what item they are purchasing
+            Console.WriteLine("What item are you purchasing? ");
+            Item = Console.ReadLine();
+
+            // Prompt: Ask the user the quantity of the item they are purchasing 
+            Console.WriteLine("What is the quantity of this item? ");
+            Quantity = Convert.ToDouble(Console.ReadLine());
+
+            // Prompt: Ask the user how much the item was
+            Console.WriteLine("How much did this item cost? ");
+            Price = Convert.ToDouble(Console.ReadLine());
+
+            // Calculate: subtotal
+            Subtotal = Quantity * Price;
+
+            // Calulate: sales tax 
+            SalesTax = Subtotal * TaxRate;
+
+            // Calculate: total 
+            Total = Subtotal + SalesTax;
+
+            // Print: end result 
+            Console.WriteLine("The subtotal for your bill is " + Subtotal.ToString("C2") + ".");
+            Console.WriteLine("The sales tax for your bill is " + SalesTax.ToString("C2") + ".");
+            Console.WriteLine("The total for your bill is " + Total.ToString("C2") + ".");
+            Console.ReadKey();
         }
     }
 }
--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -15,6 +15,36 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
+            // DECLARE: mathematical variables 
+            double FinalGrade;
+
+            // WRITE: input statements
+            Console.WriteLine("What is your first name?");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your student ID?");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for homeworks?");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for participations?");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for quizzes?");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for the midterm?");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for the final?");
+            Console.ReadLine();
+
+            // CALCULATE: final grade
+            FinalGrade = 
         }
     }
 }
